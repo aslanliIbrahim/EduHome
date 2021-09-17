6f7d7de9eb9980bab62e1c56cdffaeea14ca41ee
aslanliIbrahim <ibrahimra@code.edu.az>
2021-09-17 15:03:26 +0400

    Login complated

--- EduHome/Startup.cs
+++ EduHome/Startup.cs
@@ -1,7 +1,9 @@
 using EduHome.DAL;
+using EduHome.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,6 +33,7 @@ namespace EduHome
             {
                 options.UseSqlServer(_config["ConnectionStrings:Default"]);
             });
+            services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ EduHome/ViewModels/RegisterVm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EduHome.ViewModels
+{
+    public class RegisterVm
+    {
+        [Required, StringLength(50)]
+        public string Fullname { get; set; }
+
+        [Required, StringLength(20)]
+        public string Username { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required,DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required,DataType(DataType.Password),Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
