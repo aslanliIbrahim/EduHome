917e400097434956fa63dfa478d1529277073f63
aslanliIbrahim <ibrahimra@code.edu.az>
2021-09-26 17:32:47 +0400

    ClientSiteComplated

--- EduHome/Controllers/EventController.cs
+++ EduHome/Controllers/EventController.cs
@@ -26,7 +26,7 @@ namespace EduHome.Controllers
             };
             return View(eventVM);
         }
-        public async Task<IActionResult> Details(int? id)
+        public IActionResult Details(int? id)
         
         {
             if (id == null) return NotFound();
